6be98e376aa893e01b45d37fac9f7c1a0a28c704
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-12-30 19:52:37 +0100

    init commit

---
+++ Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseManagmentProject.Models
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
